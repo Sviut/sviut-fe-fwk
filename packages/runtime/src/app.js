fb704cfdff9a90382af0b93e8ca8305f78439507
Sviut <jeleznov88@gmail.com>
2025-09-30 19:59:04 +0300

    path

--- packages/runtime/src/app.js
+++ packages/runtime/src/app.js
@@ -1,6 +1,7 @@
 import {destroyDom} from './destroy-dom'
 import {Dispatcher} from './dispatcher'
 import {mountDom} from './mount-dom'
+import {pathDOM} from './path-dom'
 
 export function createApp({state, view, reducers = {}}) {
     let parentEl = null
@@ -23,18 +24,15 @@ export function createApp({state, view, reducers = {}}) {
     }
 
     function renderApp() {
-        if (vdom) {
-            destroyDom(vdom)
-        }
-
-        vdom = view(state, emit)
-        mountDom(vdom, parentEl)
+        const newVdom = view(state, emit)
+        vdom = pathDOM(vdom, newVdom, parentEl)
     }
 
     return {
         mount(_parentEl) {
             parentEl = _parentEl
-            renderApp()
+            vdom = view(state, emit)
+            mountDom(vdom, parentEl)
             return this
         },
 
@@ -43,7 +41,7 @@ export function createApp({state, view, reducers = {}}) {
             vdom = null
             subscriptions.forEach(unsub => unsub())
         },
-        
+
         emit(eventName, payload) {
             emit(eventName, payload)
         },
